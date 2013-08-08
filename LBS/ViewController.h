2408a6ceb0418edd95f8ef81965101ff20e7b46d
Valeriy Van <v@w7software.com>
2013-08-08 23:15:52 +0300

    Initial Commit

---
+++ LBS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LBS
+//
+//  Created by Valeriy Van on 8/8/13.
+//  Copyright (c) 2013 w7software.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LBS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LBS
+//
+//  Created by Valeriy Van on 8/8/13.
+//  Copyright (c) 2013 w7software.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
